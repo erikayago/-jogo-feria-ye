d9698de7876715b4218057a8c502d372487ddc05
gogod2134 <160232547+gogod2134@users.noreply.github.com>
2024-07-29 13:23:07 +0000

    Atualizar ID de cliente OAuth

--- cliente/js/finalFeliz.js
+++ cliente/js/finalFeliz.js
@@ -19,7 +19,7 @@ export default class finalFeliz extends Phaser.Scene {
 
     // Inicializa o Google Sign-In
     google.accounts.id.initialize({
-      client_id: '331191695151-ku8mdhd76pc2k36itas8lm722krn0u64.apps.googleusercontent.com',
+      client_id: '320391026606-f7vamcoj8312qdjgnlmvffs2tl7ud7bu.apps.googleusercontent.com',
       callback: (res) => {
         if (res.error) {
           console.error(res.error)
